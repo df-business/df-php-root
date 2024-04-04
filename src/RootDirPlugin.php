1f184a7470e96770bc9427fe242cea29787ba69f
dfer <df_business@qq.com>
2024-04-04 13:53:54 +0800

    1.4.1

--- src/RootDirPlugin.php
+++ src/RootDirPlugin.php
@@ -5,6 +5,7 @@ namespace Dfer\DfPhpRoot\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Dfer\Tools\Statics\Common;
 
 class RootDirPlugin implements PluginInterface
 {
@@ -14,15 +15,14 @@ class RootDirPlugin implements PluginInterface
         $projectRootDir = dirname($vendorDir) . DIRECTORY_SEPARATOR;
         $moduleRootDir    = $vendorDir . DIRECTORY_SEPARATOR . 'dfer' . DIRECTORY_SEPARATOR . 'df-php-root' . DIRECTORY_SEPARATOR . 'root'. DIRECTORY_SEPARATOR;
 		
-		$files = new \Dfer\Tools\Statics\Common;
         if (is_dir($moduleRootDir)) {
 			echo $moduleRootDir . "	>>>	".$projectRootDir.PHP_EOL;
             echo "////////////////////////////////////////////////// 文件复制 START //////////////////////////////////////////////////".PHP_EOL;
-            $files->copyDir($moduleRootDir, $projectRootDir);
+            Common::copyDir($moduleRootDir, $projectRootDir);
             echo "//////////////////////////////////////////////////  文件复制 END  //////////////////////////////////////////////////".PHP_EOL;
 			echo PHP_EOL;
 			echo "////////////////////////////////////////////////// 文件删除 START //////////////////////////////////////////////////".PHP_EOL;
-            $files->deleteDir($moduleRootDir);
+            Common::deleteDir($moduleRootDir);
 			echo "//////////////////////////////////////////////////  文件删除 END  //////////////////////////////////////////////////".PHP_EOL;
         }
     }
