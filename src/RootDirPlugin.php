98c1bda22b1d7a62ba76c075c1a1dc3754250155
dfer <df_business@qq.com>
2024-04-17 11:42:16 +0800

    1.4.2

--- src/RootDirPlugin.php
+++ src/RootDirPlugin.php
@@ -14,16 +14,16 @@ class RootDirPlugin implements PluginInterface
         $vendorDir  = $composer->getConfig()->get('vendor-dir');
         $projectRootDir = dirname($vendorDir) . DIRECTORY_SEPARATOR;
         $moduleRootDir    = $vendorDir . DIRECTORY_SEPARATOR . 'dfer' . DIRECTORY_SEPARATOR . 'df-php-root' . DIRECTORY_SEPARATOR . 'root'. DIRECTORY_SEPARATOR;
-		
+        
         if (is_dir($moduleRootDir)) {
-			echo $moduleRootDir . "	>>>	".$projectRootDir.PHP_EOL;
+            echo $moduleRootDir . "	>>>	".$projectRootDir.PHP_EOL;
             echo "////////////////////////////////////////////////// 文件复制 START //////////////////////////////////////////////////".PHP_EOL;
             Common::copyDir($moduleRootDir, $projectRootDir);
             echo "//////////////////////////////////////////////////  文件复制 END  //////////////////////////////////////////////////".PHP_EOL;
-			echo PHP_EOL;
-			echo "////////////////////////////////////////////////// 文件删除 START //////////////////////////////////////////////////".PHP_EOL;
+            echo PHP_EOL;
+            echo "////////////////////////////////////////////////// 文件删除 START //////////////////////////////////////////////////".PHP_EOL;
             Common::deleteDir($moduleRootDir);
-			echo "//////////////////////////////////////////////////  文件删除 END  //////////////////////////////////////////////////".PHP_EOL;
+            echo "//////////////////////////////////////////////////  文件删除 END  //////////////////////////////////////////////////".PHP_EOL;
         }
     }
 
