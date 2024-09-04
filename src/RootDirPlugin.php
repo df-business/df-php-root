6c7b10f0e82a9befbd8b4e405c5b6104736f9e0b
Dfer.Site <df_business@qq.com>
2024-09-04 16:22:18 +0800

    1.7.0

--- src/RootDirPlugin.php
+++ src/RootDirPlugin.php
@@ -1,5 +1,38 @@
 <?php
 
+/**
+ * +----------------------------------------------------------------------
+ * | 同步文件
+ * +----------------------------------------------------------------------
+ *                                            ...     .............
+ *                                          ..   .:!o&*&&&&&ooooo&; .
+ *                                        ..  .!*%*o!;.
+ *                                      ..  !*%*!.      ...
+ *                                     .  ;$$!.   .....
+ *                          ........... .*#&   ...
+ *                                     :$$: ...
+ *                          .;;;;;;;:::#%      ...
+ *                        . *@ooooo&&&#@***&&;.   .
+ *                        . *@       .@%.::;&%$*!. . .
+ *          ................!@;......$@:      :@@$.
+ *                          .@!   ..!@&.:::::::*@@*.:..............
+ *        . :!!!!!!!!!!ooooo&@$*%%%*#@&*&&&&&&&*@@$&&&oooooooooooo.
+ *        . :!!!!!!!!;;!;;:::@#;::.;@*         *@@o
+ *                           @$    &@!.....  .*@@&................
+ *          ................:@* .  ##.     .o#@%;
+ *                        . &@%..:;@$:;!o&*$#*;  ..
+ *                        . ;@@#$$$@#**&o!;:   ..
+ *                           :;:: !@;        ..
+ *                               ;@*........
+ *                       ....   !@* ..
+ *                 ......    .!%$! ..     | AUTHOR: dfer
+ *         ......        .;o*%*!  .       | EMAIL: df_business@qq.com
+ *                .:;;o&***o;.   .        | QQ: 3504725309
+ *        .;;!o&****&&o;:.    ..          | WEBSITE: http://www.dfer.site
+ * +----------------------------------------------------------------------
+ *
+ */
+
 namespace Dfer\DfPhpRoot\Composer;
 
 use Composer\Composer;
@@ -10,11 +43,11 @@ use Dfer\Tools\Statics\Common;
 class RootDirPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
-    {	
+    {
         $vendorDir  = $composer->getConfig()->get('vendor-dir');
         $projectRootDir = dirname($vendorDir) . DIRECTORY_SEPARATOR;
         $moduleRootDir    = $vendorDir . DIRECTORY_SEPARATOR . 'dfer' . DIRECTORY_SEPARATOR . 'df-php-root' . DIRECTORY_SEPARATOR . 'root'. DIRECTORY_SEPARATOR;
-        
+
         if (is_dir($moduleRootDir)) {
             echo $moduleRootDir . "	>>>	".$projectRootDir.PHP_EOL;
             echo "////////////////////////////////////////////////// 文件复制 START //////////////////////////////////////////////////".PHP_EOL;
@@ -29,11 +62,9 @@ class RootDirPlugin implements PluginInterface
 
     public function deactivate(Composer $composer, IOInterface $io)
     {
-
     }
 
     public function uninstall(Composer $composer, IOInterface $io)
     {
-
     }
 }
